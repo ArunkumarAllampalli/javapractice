edced2eaf353d98ecbc6874f7963a8d01cea2308
Arun <allamarunkumar@gmail.com>
2025-05-29 16:34:36 +0530

    simple casses added

---
+++ testproject/src/testproject/Simple.java
@@ -0,0 +1,9 @@
+package testproject;
+
+public class Simple {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is Sample java Program");
+
+	}
+}
