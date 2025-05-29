90b5b6f91b9f10e4a6f2b624205371a684498e05
Arun <allamarunkumar@gmail.com>
2025-05-29 16:36:51 +0530

    simple line added

--- testproject/src/testproject/Simple.java
+++ testproject/src/testproject/Simple.java
@@ -3,7 +3,8 @@ package testproject;
 public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi this is Sample java Program");
+		System.out.println("Hi this is Sample java Program.");
+		System.out.println("This welcome to Simple classes.");
 
 	}
 }
